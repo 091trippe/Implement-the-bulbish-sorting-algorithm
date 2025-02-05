964175e0ff838d2d2621e38516fbd37afa63cf8e
TRIPPE <TRIPPE@KOMPUTER>
2025-02-05 12:54:07 +0200

    Добавьте файлы проекта.

---
+++ Implement the bulbish sorting algorithm/Implement the bulbish sorting algorithm.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cstdlib>  
+
+using namespace std;
+
+
+// Тут проблема в тому що чомусь не считує кирилицю але код робочий
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    srand(time(nullptr)); 
+
+    int n;
+    cout << "Введіть розмір масиву: "; 
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Розмір масиву має бути більше нуля!" << std::endl;
+        return 1;
+    }
+
+    int* arr = new int[n]; 
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100 + 1;
+    }
+
+    cout << "Згенерований масив: ";
+    printArray(arr, n);
+
+    
+    bubbleSort(arr, n);
+
+    cout << "Відсортований масив: ";
+    printArray(arr, n);
+
+    delete[] arr;
+
+    return 0;
+}
